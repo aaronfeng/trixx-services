b3eba577bc66d942f80787e84bd83d043584018a
Aaron Feng <aaron.feng@gmail.com>
2009-09-26 19:30:47 -0400

    call trixx/init to read erlang cookie

--- src/main/clj/com/leftrightfold/trixx/services/main.clj
+++ src/main/clj/com/leftrightfold/trixx/services/main.clj
@@ -1,10 +1,12 @@
 (ns com.leftrightfold.trixx.services.main
-  (:use com.leftrightfold.trixx.services.core)
-  (:use compojure)
+  (:require [com.leftrightfold.trixx.core :as t]
+            [com.leftrightfold.trixx.services.core :as ts])
+  (:require [compojure :as compojure])
   (:gen-class))
 
 (defn -main [& args]
   (prn "Welcome to Trixx services Repl")
-  (run-server {:port 8080}
-              "/*" (servlet webservice))
+  (t/init)
+  (compojure/run-server {:port 8080}
+              "/*" (compojure/servlet ts/webservice))
   (clojure.lang.Repl/main (into-array String args)))
